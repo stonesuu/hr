88b504a8d7968d8fb3bf15353117cae51a092198
stonesuu <365549445@qq.com>
2017-06-01 17:11:33 +0800

    2017-06-01 at work

---
+++ main.py
@@ -0,0 +1,413 @@
+#!/bin/env python2.7
+#coding=utf-8
+import os,xlrd
+import datetime
+from flask import Flask,session,request,render_template,redirect,json,url_for
+import dbutil,time
+from werkzeug import secure_filename
+try:
+	conn = dbutil.DB('hr','10.99.160.36','root','root')
+	conn.connect()
+except Exception as e:
+	print e
+	print 'connect error'
+else:
+	print 'connect success'
+SN_dict = {}
+date_list = []
+UPLOAD_FOLDER = 'D:\upload'
+ALLOWED_EXTENSIONS = set(['xls','xlsx'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.secret_key = 'sadfagraegrgaregareghhqare'
+
+
+def getjson(sql):
+	try:
+		tmp = conn.execute(sql)
+		res = json.dumps(tmp)
+	except Exception as e:
+		return 'error'
+	else:
+		return res
+def allowed_file(filename):
+	return '.' in filename and \
+		filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
+
+def excel_to_db_stuff(file):
+	book = xlrd.open_workbook(file)
+	sheet = book.sheet_by_index(0)
+	col_num = sheet.ncols
+	row_num = sheet.nrows
+	if col_num == 0:
+		print 'error,the column is %s' % (col_num)
+		return '-1'
+	else:
+		print 'the column is %s' % (col_num,)
+		val = ''
+		for i in range(0,col_num):
+			val += '%s,'
+		dfun = []
+		for i in range(1,row_num):
+			row_values = sheet.row_values(i) 
+			in_work_date = xlrd.xldate.xldate_as_datetime(sheet.cell(i,14).value,0)
+			in_company_date = xlrd.xldate.xldate_as_datetime(sheet.cell(i,21).value,0)
+			# Convert an Excel date/time number into a datetime.datetime object.  
+			# @param xldate The Excel number  
+			# @param datemode 0: 1900-based, 1: 1904-based.  
+			row_values[14] = in_work_date
+			row_values[21] = in_company_date
+			#print row_values
+			dfun.append(row_values)
+		sql = '''insert into stuff(name,pre_name,sex,MZ,id,ZZMM,learn,health,HKLB,
+			merriage,JG,HKSZD,home_phone,cell_phone,in_work_date,home_add1,home_add2,company,job_status,department,
+			job_name,in_company_date,LWPQGS,JT_department,RYXL,conn_name,conn_phone,JTXZ,
+			XSBZ,GZBZ,base_salary,high_temp_base,special_job_base,night_job_base,leader_salary,age_salary,meal_base
+			,SB_base,YB_base,env_salary,switch,other,order_num) values ('''
+		#cursor.executemany("insert into resources_networkdevice values("+val[:-1]+");" ,dfun)
+		try: 
+			conn.cursor.execute('delete from stuff')
+			#conn.cursor.execute('truncate table stuff;')
+			conn.cursor.executemany(sql+val[:-1]+");",dfun)
+			conn.cursor.execute('commit')
+		except Exception as e:
+			print e
+			return '-1'
+		else:
+			return '1'
+def excel_to_check(file):
+	book = xlrd.open_workbook(file)
+	sheet = book.sheet_by_index(0)
+	col_num = sheet.ncols
+	row_num = sheet.nrows
+	try:
+		conn.cursor.execute('start transaction')
+		for i in range(1,row_num):
+			row_values = sheet.row_values(i)
+			row_values_change = row_values[3:]
+			row_values_change.append(row_values[2])
+			#print row_values_change
+			row_values_tuple = tuple(row_values_change)
+			#print row_values_tuple
+			sql = '''update check_once set BJ=%s,SJ=%s,KG=%s,TQJ=%s,HSJ=%s,CJ=%s,GSJ=%s,QTJ=%s,
+			JHSYJ=%s,GJ=%s,NXJ=%s,full_night_job=%s,holiday_job=%s,sunday_job=%s,add_job=%s,
+			JT_days=%s,reward_salary=%s,check_salary=%s,BX_check=%s where id="%s"''' % row_values_tuple
+			#print sql
+			conn.cursor.execute(sql)
+	except Exception as e:
+		print e
+		return '-1'
+	else:
+		conn.cursor.execute('commit')
+		return '1'
+
+def get_time():
+	cur = datetime.datetime.now()
+	year = cur.year
+	month = cur.month
+	day = cur.day
+	if month != 1:
+		return ('%s-%s-%s' %(year,month,day),'%s-%s-01' %(year,month-1))
+	else:
+		return ('%s-%s-%S' %(year,month,day),'%s-12-01' %(year-1,))
+
+
+
+
+
+@app.route('/upload',methods=['GET','POST'])
+def upload():
+	if request.method == 'GET':
+		return render_template('upload.html')
+	else:
+		file = request.files['file']
+    	if file and allowed_file(file.filename):
+        	#filename = secure_filename(file.filename)
+        	filename = file.filename
+        	ab_path_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            #return redirect(url_for('uploaded_file',filename=filename))
+        	res = excel_to_db_stuff(ab_path_file)
+        	return res
+
+# @app.route('/common/salary/upload',methods=['POST'])
+# def com_sal_up():
+# 	file = request.files['check_file']
+# 	print file
+# 	if file and allowed_file(file.filename):
+# 		file = file.name
+# 		ab_path_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+#     	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#         #return redirect(url_for('uploaded_file',filename=filename))
+#         return 'ok'
+
+@app.route('/')
+def login_r():
+	return redirect('/signin')
+
+@app.route('/signin',methods=['GET','POST'])
+def signin():
+	if request.method == 'GET':
+		return render_template('signin.html')
+	elif request.method == 'POST':
+		username = request.form.get('username')
+		password = request.form.get('password')
+		if username == 'root':
+			session['user'] = 'root'
+			return redirect('/common/stuff')
+	# 	sql = "SELECT COMPANY FROM user WHERE (NAME='%s' AND PASSWORD='%s')" % (username,password)
+	# 	res = conn.execute(sql)
+	# 	if len(res) == 0:
+	# 		return render_template('signin.html',message="error")
+	# 	else:
+	# 		session['user'] = username
+	# 		session['group'] = res[0][0]
+	# 		if session['group'] == 0:
+	# 			return redirect('/super/user')
+	# 		else:
+	# 			return redirect('/common/stuff')
+
+@app.route('/logout')
+def logout():
+	session.pop('user')
+	session.pop('group')
+	return redirect('/signin')
+
+
+# @app.route('/signup')
+# def signup():
+# 	return render_template('signup.html')
+
+# @app.route('/common/getC',methods=['GET','POST'])
+# def com_getC():
+# 	if request.method == 'GET':
+# 		sql = 'SELECT NAME FROM COMPANY'
+# 		res = getjson(sql)
+# 		return res
+# 	else:
+# 		sql = 'SELECT NAME FROM COMPANY WHERE ID_C=%s' %(session['group'])
+# 		res = conn.execute(sql)
+# 		return res[0][0]
+
+# @app.route('/common/signup',methods=['post'])
+# def com_signup():
+# 	username = request.form.get('username')
+# 	password = request.form.get('password')
+# 	company = request.form.get('company')
+# 	sql = "INSERT INTO user(NAME,PASSWORD,COMPANY) SELECT '%s','%s',ID_C FROM company WHERE NAME='%s'" %(username,password,company)
+# 	res = conn.execute(sql)
+# 	if not res:
+# 		return 'ok'
+# 	else:
+# 		return 'error'
+
+
+@app.route('/common/stuff')
+def com_stuff():
+	if 'user' in session:
+		return render_template('stuff.html')
+	else:
+		return redirect('/signin')
+
+# @app.route('/common/stuff/getmessage')
+# def com_stuff_getm():
+# 	print session['user']
+# 	return json.dumps((session['user'],session['group']))
+
+# @app.route('/common/stuff/add',methods=['POST'])
+# def com_stuff_add():
+# 	name = request.form.get('name')
+# 	age = int(request.form.get('age'))
+# 	sex = request.form.get('sex')
+# 	tele = int(request.form.get('tele'))
+# 	address = request.form.get('address')
+# 	company = int(request.form.get('company'))
+# 	salary = int(request.form.get('salary'))
+# 	sql = "INSERT INTO stuff(NAME,SEX,AGE,COMPANY,TEL,ADDRESS,SALARY) VALUES ('%s','%s',%s,%s,'%s','%s',%s)" %(name,sex,age,company,tele,address,salary)
+# 	res = conn.execute(sql)
+# 	if not res:
+# 		return 'ok'
+# 	else:
+# 		return 'error'
+
+@app.route('/common/stuff/get')
+def com_stuff_get():
+	res_for_json = [] 
+	year = datetime.datetime.now().year
+	sql = 'select name,sex,id,company,job_name,GZBZ from stuff order by order_num'
+	res = conn.execute(sql)
+	for tmp_tuple in res:
+		tmp_id = tmp_tuple[2]
+		birth = '.'.join([tmp_id[6:10],tmp_id[10:12],tmp_id[12:14]])
+		age = str(year - int(tmp_id[6:10]))
+		tmp_res_for_json = (tmp_tuple[0],age,tmp_tuple[1],tmp_tuple[2],tmp_tuple[3],tmp_tuple[4],tmp_tuple[5])
+		#print tmp_res_for_json
+		res_for_json.append(tmp_res_for_json)
+	return json.dumps(res_for_json)
+
+# @app.route('/common/stuff/del',methods=['POST'])
+# def com_stuff_del():
+# 	ID_I = request.form.get('id')
+# 	sql = "DELETE FROM stuff WHERE ID_I=%s" % (ID_I)
+# 	res = conn.execute(sql)
+# 	if not res:
+# 		return 'ok'
+# 	else:
+# 		return 'error'
+
+# @app.route('/common/stuff/update',methods=['POST'])
+# def com_stuff_upd():
+# 	ID_I = request.form.get('id')
+# 	name = request.form.get('name')
+# 	sex = request.form.get('sex')
+# 	age = request.form.get('age')
+# 	tele = request.form.get('tele')
+# 	address = request.form.get('address')
+# 	salary = request.form.get('salary')
+# 	sql = "UPDATE stuff SET NAME='%s',SEX='%s',AGE=%s,TEL='%s',ADDRESS='%s',SALARY=%s WHERE ID_I=%s" %(name,sex,age,tele,address,salary,ID_I)
+# 	res = conn.execute(sql)
+# 	if not res:
+# 		return 'ok'
+# 	else:
+# 		return 'error'
+
+# @app.route('/common/salary/getime')
+# def com_salary_gt():
+# 	sql = 'SELECT TIMELOK FROM company_lock WHERE ID_C=%s' %(session['group'],)
+# 	res = conn.execute(sql)
+# 	#print type(res[0][0])
+# 	date = res[0][0].strftime('%Y-%m-%d')
+# 	return date
+	
+@app.route('/common/salary',methods=['GET','POST'])
+def com_salary():
+	if request.method == 'POST':
+		file = request.files['check_file']
+		#print file
+		if file and allowed_file(file.filename):
+			filename = file.filename
+			print filename
+			ab_path_file = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+	    	file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+	    	res = excel_to_check(ab_path_file)
+	    	if res == '1':
+	    		session['check_upload'] = 1
+	        	return render_template('salary_after.html')
+	        else:
+	        	return 'error'
+	else:
+		if 'user' in session:
+			if 'check_upload' in session:
+				return redirect('/common/salary/check_file')
+			else:
+				return render_template('salary_before.html')
+		else:
+			return redirect('/signin')
+
+@app.route('/common/salary/reupload')
+def com_sal_reup():
+	session.pop('check_upload')
+	return redirect('/common/salary')
+
+@app.route('/common/salary/check_file',methods=['GET','POST'])
+def com_sal_file():
+	if request.method == 'POST':
+		pass
+	else:
+		return render_template('salary_after.html')
+
+@app.route('/common/salary/getbase')
+def com_salary_gb():
+	sql = '''select name,c.id,BJ,SJ,KG,TQJ,HSJ,CJ,GSJ,QTJ,JHSYJ,GJ,NXJ,
+	full_night_job,holiday_job,sunday_job,add_job,JT_days,reward_salary,check_salary,
+	BX_check from check_once c join stuff s on c.id=s.id order by s.order_num
+	'''
+	tmp = conn.execute(sql)
+	res = json.dumps(tmp)
+	return res
+
+@app.route('/common/salary/set',methods=['POST'])
+def com_salary_set():
+	ID_I = request.form.get('id')
+	col = request.form.get('col')
+	val = request.form.get('val')
+	sql = "UPDATE set_i SET %s=%s WHERE ID=%s" % (col,val,ID_I)
+	res = conn.execute(sql)
+	if not res:
+		return 'ok'
+	else:
+		return 'error'
+
+@app.route('/common/salary/cal',methods=['GET','POST'])
+def com_salary_cal():
+	if request.method == 'POST':
+		sql = "call hr_salary_cal()"
+		res = conn.execute(sql)
+		if not res:
+			return 'ok'
+		else:
+			return 'error'
+	else:
+		return render_template('salary_cal.html')
+
+@app.route('/common/salary/get_cal')
+def com_salary_gc():
+	sql = '''select name,t.id,ZG,a.base_salary,salary_XJ,check_salary,high_temp_salary,
+	special_job_salary,a.leader_salary,add_job_salary,a.age_salary,night_job_salary,meal_salary,
+	a.env_salary,reward_salary,salary_get,a.SB_base,a.YB_base,YL_C,YL_I,SY_C,SY_I,GS_C,YB_C,YB_I,
+	BI_C,SI_C,BX_check,BX_XJ,tex,salary_sum from salary_once a join stuff t on a.id=t.id order by t.order_num;
+	'''
+	res = getjson(sql)
+	return res
+
+@app.route('/common/salary/submit',methods=['POST'])
+def com_salary_sub():
+	time = get_time()[1]
+	sql01 = 'call hr_check_all("%s")' %(time,)
+	sql02 = 'call hr_salary_all("%s")' %(time,)
+	sql03 = '''update check_once set BJ=0,SJ=0,KG=0,TQJ=0,HSJ=0,CJ=0,GSJ=0,QTJ=0,
+			JHSYJ=0,GJ=0,NXJ=0,full_night_job=0,holiday_job=0,sunday_job=0,add_job=0,
+			JT_days=0,reward_salary=0,check_salary=0,BX_check=0'''
+	print sql01,sql02
+	try:
+		conn.cursor.execute('start transaction')
+		conn.cursor.execute(sql01)
+		conn.cursor.execute(sql02)
+		conn.cursor.execute(sql03)
+	except Exception as e:
+		conn.cursor.execute('rollback')
+		print e
+		return 'error'
+	else:
+		conn.cursor.execute('commit')
+		session.pop('check_upload')
+		return 'ok'
+@app.route('/common/summary/check')
+def com_sum_chk():
+	return 'pass'
+
+@app.route('/common/summary/salary')
+def com_sum_sal():
+	return 'pass'
+
+@app.route('/common/summary/table',methods=['GET','POST'])
+def com_sum_table():
+	if request.method == 'GET':
+		if 'user' in session:
+			return render_template('summary_table.html')
+
+@app.route('/common/set',methods=['GET','POST'])
+def com_set():
+	if request.method == 'GET':
+		if 'user' in session:
+			return render_template('common_set.html')
+@app.route('/common/set/depart_get')
+def com_set_depget():
+	sql = 'select id,name from department'
+	res = getjson(sql)
+	return res
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=9023,debug=True)
---
+++ procedure.sql
@@ -0,0 +1,265 @@
+create procedure hr_salary_cal()
+begin
+declare id_chk varchar(20);
+declare base_salary_stf float(7,2);
+declare high_temp_base_stf float(7,2);
+declare special_job_base_stf float(7,2);
+declare night_job_base_stf float(7,2);
+declare leader_salary_stf float(7,2);
+declare age_salary_stf float(7,2);
+declare meal_base_stf float(7,2);
+declare env_salary_stf float(7,2);
+declare SB_base_stf float(7,2);
+declare YB_base_stf float(7,2);
+declare switch_stf int;
+declare BJ_chk float(4,2);
+declare SJ_chk float(4,2);
+declare KG_chk float(4,2);
+declare TQJ_chk float(4,2);
+declare HSJ_chk float(4,2);
+declare CJ_chk float(4,2);
+declare GSJ_chk float(4,2);
+declare QTJ_chk float(4,2);
+declare JHSYJ_chk float(4,2);
+declare GJ_chk float(4,2);
+declare full_night_job_chk float(4,2);
+declare holiday_job_chk float(4,2);
+declare sunday_job_chk float(4,2);
+declare add_job_chk float(4,2);
+declare JT_days_chk float(4,2);
+declare reward_salary_chk float(7,2);
+declare check_salary_chk float(7,2);
+declare BX_check_chk float(7,2);
+declare ZG_sal float(4,2);
+declare base_salary_sal float(7,2);
+declare salary_XJ_sal float(7,2);
+declare check_salary_sal float(7,2);
+declare high_temp_salary_sal float(7,2);
+declare special_job_salary_sal float(7,2);
+declare leader_salary_sal float(7,2);
+declare add_job_salary_sal float(7,2);
+declare age_salary_sal float(7,2);
+declare night_job_salary_sal float(7,2);
+declare meal_salary_sal float(7,2);
+declare env_salary_sal float(7,2);
+declare reward_salary_sal float(7,2);
+declare salary_get_sal float(7,2);
+declare SB_base_sal float(7,2);
+declare YB_base_sal float(7,2);
+declare YL_C_sal float(7,2);
+declare YL_I_sal float(7,2);
+declare SY_C_sal float(7,2);
+declare SY_I_sal float(7,2);
+declare GS_C_sal float(7,2);
+declare YB_C_sal float(7,2);
+declare YB_I_sal float(7,2);
+declare BI_C_sal float(7,2);
+declare SI_C_sal float(7,2);
+declare BX_check_sal float(7,2);
+declare BX_XJ_sal float(7,2);
+declare tex_sal float(7,2);
+declare salary_sum_sal float(7,2);
+declare done int default 0;
+declare cur01 cursor for (select id,BJ,SJ,KG,TQJ,HSJ,CJ,GSJ,QTJ,JHSYJ,GJ,full_night_job,holiday_job,sunday_job,add_job,JT_days,reward_salary,check_salary,BX_check from check_once);
+declare continue handler for sqlstate '02000' set done = 1;
+start transaction;
+delete from salary_once;
+open cur01;
+fetch cur01 into id_chk,BJ_chk,SJ_chk,KG_chk,TQJ_chk,HSJ_chk,CJ_chk,GSJ_chk,QTJ_chk,JHSYJ_chk,GJ_chk,full_night_job_chk,holiday_job_chk,sunday_job_chk,add_job_chk,JT_days_chk,reward_salary_chk,check_salary_chk,BX_check_chk;
+while not done do
+select base_salary,high_temp_base,special_job_base,night_job_base,leader_salary,age_salary,meal_base,env_salary,SB_base,YB_base,switch into base_salary_stf,high_temp_base_stf,special_job_base_stf,night_job_base_stf,leader_salary_stf,age_salary_stf,meal_base_stf,env_salary_stf,SB_base_stf,YB_base_stf,switch_stf from stuff where id=id_chk;
+set ZG_sal = 21.75-BJ_chk-SJ_chk-KG_chk-TQJ_chk-GSJ_chk-QTJ_chk-JHSYJ_chk-GJ_chk;
+set base_salary_sal = base_salary_stf;
+set salary_XJ_sal = 1.5*base_salary_sal*(ZG_sal+BJ_chk*0.6)/21.75;
+set check_salary_sal = check_salary_chk;
+set high_temp_salary_sal = high_temp_base_stf*JT_days_chk;
+set special_job_salary_sal = special_job_base_stf*JT_days_chk;
+set leader_salary_sal = leader_salary_stf;
+set add_job_salary_sal = 1.5*base_salary_sal*(3*holiday_job_chk+2*sunday_job_chk+1.5*add_job_chk)/21.75;
+if (BJ_chk+SJ_chk+KG_chk+TQJ_chk+HSJ_chk+CJ_chk+GSJ_chk+QTJ_chk+JHSYJ_chk+GJ_chk)>=10 then
+set age_salary_sal = 0;
+set env_salary_sal = 0;
+elseif (BJ_chk+SJ_chk+KG_chk+TQJ_chk+HSJ_chk+CJ_chk+GSJ_chk+QTJ_chk+JHSYJ_chk+GJ_chk)>=5 then
+set age_salary_sal = age_salary_stf*0.5;
+set env_salary_sal = env_salary_stf*0.5;
+else
+set age_salary_sal = age_salary_stf;
+set env_salary_sal = env_salary_stf;
+end if;
+set night_job_salary_sal = night_job_base_stf*JT_days_chk;
+set meal_salary_sal = meal_base_stf*JT_days_chk;
+set reward_salary_sal = reward_salary_chk;
+set salary_get_sal = salary_XJ_sal+check_salary_sal+high_temp_salary_sal+special_job_salary_sal+leader_salary_sal+add_job_salary_sal+age_salary_sal+night_job_salary_sal+meal_salary_sal+reward_salary_sal;
+set SB_base_sal = SB_base_stf;
+set YB_base_sal = YB_base_stf;
+set YL_C_sal = SB_base_sal*0.2;
+set YL_I_sal = SB_base_sal*0.08;
+set SY_C_sal = SB_base_sal*0.01;
+set SY_I_sal = SB_base_sal*0.005;
+set GS_C_sal = SB_base_sal*0.015;
+set YB_C_sal = YB_base_sal*0.07;
+set YB_I_sal = YB_base_sal*0.02;
+set BI_C_sal = YB_base_sal*0.005;
+set SI_C_sal = 11;
+set BX_check_sal = BX_check_chk;
+set BX_XJ_sal = YL_I_sal+SY_I_sal+YB_I_sal;
+if (salary_get_sal-BX_XJ_sal)<=3500 then
+set tex_sal = 0;
+elseif (salary_get_sal-BX_XJ_sal)<=5000 then
+set tex_sal = (salary_get_sal-BX_XJ_sal-3500)*0.03;
+elseif (salary_get_sal-BX_XJ_sal)<=8000 then
+set tex_sal = (salary_get_sal-BX_XJ_sal-5000)*0.1+105+45;
+else
+set tex_sal = (salary_get_sal-BX_XJ_sal-8000)*0.2+555+450+45;
+end if;
+set salary_sum_sal = salary_get_sal-BX_XJ_sal-tex_sal;
+insert into salary_once(id,ZG,base_salary,salary_XJ,check_salary,high_temp_salary,special_job_salary,leader_salary,add_job_salary,age_salary,night_job_salary,meal_salary,env_salary,reward_salary,salary_get,SB_base,YB_base,YL_C,YL_I,SY_C,SY_I,GS_C,YB_C,YB_I,BI_C,SI_C,BX_check,BX_XJ,tex,salary_sum) values (id_chk,ZG_sal,base_salary_sal,salary_XJ_sal,check_salary_sal,high_temp_salary_sal,special_job_salary_sal,leader_salary_sal,add_job_salary_sal,age_salary_sal,night_job_salary_sal,meal_salary_sal,env_salary_sal,reward_salary_sal,salary_get_sal,SB_base_sal,YB_base_sal,YL_C_sal,YL_I_sal,SY_C_sal,SY_I_sal,GS_C_sal,YB_C_sal,YB_I_sal,BI_C_sal,SI_C_sal,BX_check_sal,BX_XJ_sal,tex_sal,salary_sum_sal);
+fetch cur01 into id_chk,BJ_chk,SJ_chk,KG_chk,TQJ_chk,HSJ_chk,CJ_chk,GSJ_chk,QTJ_chk,JHSYJ_chk,GJ_chk,full_night_job_chk,holiday_job_chk,sunday_job_chk,add_job_chk,JT_days_chk,reward_salary_chk,check_salary_chk,BX_check_chk;
+end while;
+close cur01;
+commit;
+end//
+
+
+
+create procedure hr_check_all(date_sub varchar(20))
+begin
+create temporary table check_tmp select * from check_once;
+alter table check_tmp add column date datetime;
+update check_tmp set date=date_sub;
+delete from check_all where date=date_sub;
+insert into check_all select * from check_tmp;
+drop table check_tmp;
+end //
+
+create procedure hr_salary_all(date_sub varchar(20))
+begin
+create temporary table salary_tmp select * from salary_once;
+alter table salary_tmp add column date datetime;
+update salary_tmp set date=date_sub;
+delete from salary_all where date=date_sub;
+insert into salary_all select * from salary_tmp;
+drop table salary_tmp;
+end //
+
+create procedure hr_salary_cal()
+begin
+declare id_chk varchar(20);
+declare base_salary_stf float(7,2);
+declare high_temp_base_stf float(7,2);
+declare special_job_base_stf float(7,2);
+declare night_job_base_stf float(7,2);
+declare leader_salary_stf float(7,2);
+declare age_salary_stf float(7,2);
+declare meal_base_stf float(7,2);
+declare env_salary_stf float(7,2);
+declare SB_base_stf float(7,2);
+declare YB_base_stf float(7,2);
+declare switch_stf int;
+declare BJ_chk float(4,2);
+declare SJ_chk float(4,2);
+declare KG_chk float(4,2);
+declare TQJ_chk float(4,2);
+declare HSJ_chk float(4,2);
+declare CJ_chk float(4,2);
+declare GSJ_chk float(4,2);
+declare QTJ_chk float(4,2);
+declare JHSYJ_chk float(4,2);
+declare GJ_chk float(4,2);
+declare full_night_job_chk float(4,2);
+declare holiday_job_chk float(4,2);
+declare sunday_job_chk float(4,2);
+declare add_job_chk float(4,2);
+declare JT_days_chk float(4,2);
+declare reward_salary_chk float(7,2);
+declare check_salary_chk float(7,2);
+declare BX_check_chk float(7,2);
+declare ZG_sal float(4,2);
+declare base_salary_sal float(7,2);
+declare salary_XJ_sal float(7,2);
+declare check_salary_sal float(7,2);
+declare high_temp_salary_sal float(7,2);
+declare special_job_salary_sal float(7,2);
+declare leader_salary_sal float(7,2);
+declare add_job_salary_sal float(7,2);
+declare age_salary_sal float(7,2);
+declare night_job_salary_sal float(7,2);
+declare meal_salary_sal float(7,2);
+declare env_salary_sal float(7,2);
+declare reward_salary_sal float(7,2);
+declare salary_get_sal float(7,2);
+declare SB_base_sal float(7,2);
+declare YB_base_sal float(7,2);
+declare YL_C_sal float(7,2);
+declare YL_I_sal float(7,2);
+declare SY_C_sal float(7,2);
+declare SY_I_sal float(7,2);
+declare GS_C_sal float(7,2);
+declare YB_C_sal float(7,2);
+declare YB_I_sal float(7,2);
+declare BI_C_sal float(7,2);
+declare SI_C_sal float(7,2);
+declare BX_check_sal float(7,2);
+declare BX_XJ_sal float(7,2);
+declare tex_sal float(7,2);
+declare salary_sum_sal float(7,2);
+declare done int default 0;
+declare cur01 cursor for (select id,BJ,SJ,KG,TQJ,HSJ,CJ,GSJ,QTJ,JHSYJ,GJ,full_night_job,holiday_job,sunday_job,add_job,JT_days,reward_salary,check_salary,BX_check from check_once);
+declare continue handler for sqlstate '02000' set done = 1;
+start transaction;
+delete from salary_once;
+open cur01;
+fetch cur01 into id_chk,BJ_chk,SJ_chk,KG_chk,TQJ_chk,HSJ_chk,CJ_chk,GSJ_chk,QTJ_chk,JHSYJ_chk,GJ_chk,full_night_job_chk,holiday_job_chk,sunday_job_chk,add_job_chk,JT_days_chk,reward_salary_chk,check_salary_chk,BX_check_chk;
+while not done do
+select base_salary,high_temp_base,special_job_base,night_job_base,leader_salary,age_salary,meal_base,env_salary,SB_base,YB_base,switch into base_salary_stf,high_temp_base_stf,special_job_base_stf,night_job_base_stf,leader_salary_stf,age_salary_stf,meal_base_stf,env_salary_stf,SB_base_stf,YB_base_stf,switch_stf from stuff where id=id_chk;
+set ZG_sal = 21.75-BJ_chk-SJ_chk-KG_chk-TQJ_chk-GSJ_chk-QTJ_chk-JHSYJ_chk-GJ_chk;
+set base_salary_sal = base_salary_stf;
+set salary_XJ_sal = 1.5*base_salary_sal*(ZG_sal+BJ_chk*0.6)/21.75;
+set check_salary_sal = check_salary_chk;
+set high_temp_salary_sal = high_temp_base_stf*JT_days_chk;
+set special_job_salary_sal = special_job_base_stf*JT_days_chk;
+set leader_salary_sal = leader_salary_stf;
+set add_job_salary_sal = 1.5*base_salary_sal*(3*holiday_job_chk+2*sunday_job_chk+1.5*add_job_chk)/21.75;
+if (BJ_chk+SJ_chk+KG_chk+TQJ_chk+HSJ_chk+CJ_chk+GSJ_chk+QTJ_chk+JHSYJ_chk+GJ_chk)>=10 then
+set age_salary_sal = 0;
+set env_salary_sal = 0;
+elseif (BJ_chk+SJ_chk+KG_chk+TQJ_chk+HSJ_chk+CJ_chk+GSJ_chk+QTJ_chk+JHSYJ_chk+GJ_chk)>=5 then
+set age_salary_sal = age_salary_stf*0.5;
+set env_salary_sal = env_salary_stf*0.5;
+else
+set age_salary_sal = age_salary_stf;
+set env_salary_sal = env_salary_stf;
+end if;
+set night_job_salary_sal = night_job_base_stf*JT_days_chk;
+set meal_salary_sal = meal_base_stf*JT_days_chk;
+set reward_salary_sal = reward_salary_chk;
+set salary_get_sal = salary_XJ_sal+check_salary_sal+high_temp_salary_sal+special_job_salary_sal+leader_salary_sal+add_job_salary_sal+age_salary_sal+night_job_salary_sal+meal_salary_sal+reward_salary_sal;
+set SB_base_sal = SB_base_stf;
+set YB_base_sal = YB_base_stf;
+set YL_C_sal = SB_base_sal*0.2;
+set YL_I_sal = SB_base_sal*0.08;
+set SY_C_sal = SB_base_sal*0.01;
+set SY_I_sal = SB_base_sal*0.005;
+set GS_C_sal = SB_base_sal*0.015;
+set YB_C_sal = YB_base_sal*0.07;
+set YB_I_sal = YB_base_sal*0.02;
+set BI_C_sal = YB_base_sal*0.005;
+set SI_C_sal = 11;
+set BX_check_sal = BX_check_chk;
+set BX_XJ_sal = YL_I_sal+SY_I_sal+YB_I_sal;
+if (salary_get_sal-BX_XJ_sal)<=3500 then
+set tex_sal = 0;
+elseif (salary_get_sal-BX_XJ_sal)<=5000 then
+set tex_sal = (salary_get_sal-BX_XJ_sal-3500)*0.03;
+elseif (salary_get_sal-BX_XJ_sal)<=8000 then
+set tex_sal = (salary_get_sal-BX_XJ_sal-5000)*0.1+105+45;
+else
+set tex_sal = (salary_get_sal-BX_XJ_sal-8000)*0.2+555+450+45;
+end if;
+set salary_sum_sal = salary_get_sal-BX_XJ_sal-tex_sal;
+insert into salary_once(id,ZG,base_salary,salary_XJ,check_salary,high_temp_salary,special_job_salary,leader_salary,add_job_salary,age_salary,night_job_salary,meal_salary,env_salary,reward_salary,salary_get,SB_base,YB_base,YL_C,YL_I,SY_C,SY_I,GS_C,YB_C,YB_I,BI_C,SI_C,BX_check,BX_XJ,tex,salary_sum) values (id_chk,ZG_sal,base_salary_sal,salary_XJ_sal,check_salary_sal,high_temp_salary_sal,special_job_salary_sal,leader_salary_sal,add_job_salary_sal,age_salary_sal,night_job_salary_sal,meal_salary_sal,env_salary_sal,reward_salary_sal,salary_get_sal,SB_base_sal,YB_base_sal,YL_C_sal,YL_I_sal,SY_C_sal,SY_I_sal,GS_C_sal,YB_C_sal,YB_I_sal,BI_C_sal,SI_C_sal,BX_check_sal,BX_XJ_sal,tex_sal,salary_sum_sal);
+fetch cur01 into id_chk,BJ_chk,SJ_chk,KG_chk,TQJ_chk,HSJ_chk,CJ_chk,GSJ_chk,QTJ_chk,JHSYJ_chk,GJ_chk,full_night_job_chk,holiday_job_chk,sunday_job_chk,add_job_chk,JT_days_chk,reward_salary_chk,check_salary_chk,BX_check_chk;
+end while;
+close cur01;
+commit;
+end//
